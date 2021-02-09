439618fc700a5a9f38f4c895046d8568b4ea9113
Julio <juliomederosarias@gmail.com>
2021-02-09 17:52:40 -0300

    check

--- menu/models.py
+++ menu/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-import datetime
+import datetime,uuid
 from django.core.validators import MinValueValidator
 
 # Create your models here.
 
 class menu(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     option1=models.CharField(max_length=100)
     option2=models.CharField(max_length=100)
     option3=models.CharField(max_length=100)
